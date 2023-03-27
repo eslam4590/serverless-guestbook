aff8b8503ae7df7c44e46795fd921f3a4058df3a
eslam4590 <eslamessam232@gmail.com>
2023-03-27 13:39:16 -0400

    updated guestbook

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/ec209a2e-a3f1-433d-852d-7c9c1a54f851/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save-guestbook-entry-sequence.jason`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
